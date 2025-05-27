443743bc429b01a0fbc5e0310ed7c1b2503c82c6
Wang-Simona <wyingxuan9@gmail.com>
2025-05-27 10:51:49 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,47 @@
+/** @type {SVGImage[]} */
+let lettere_svg = [];
+ 
+/** @type {SVGImage[]} */
+let forme_svg = [];
+
+function preload() {
+  lettere_svg = [
+    loadSVG("./Svgs/shapes/1.svg")
+    loadSVG("./Svgs/shapes/6.svg")
+    loadSVG("./Svgs/shapes/11.svg")
+    loadSVG("./Svgs/shapes/16.svg")
+    loadSVG("./Svgs/shapes/21.svg")
+    loadSVG("./Svgs/shapes/26.svg")
+    loadSVG("./Svgs/shapes/31.svg")
+    loadSVG("./Svgs/shapes/36.svg")
+    loadSVG("./Svgs/shapes/41.svg")
+    loadSVG("./Svgs/shapes/46.svg")
+    loadSVG("./Svgs/shapes/51.svg")
+    loadSVG("./Svgs/shapes/56.svg")
+    loadSVG("./Svgs/shapes/60.svg")
+    loadSVG("./Svgs/shapes/66.svg")
+    loadSVG("./Svgs/shapes/71.svg")
+    
+  ]
+}
+
+function preload() {
+  forme_svg = [
+    loadSVG("./Svgs/letters/字母à.svg")
+    loadSVG("./Svgs/letters/字母B.svg")
+    loadSVG("./Svgs/letters/字母E.svg")
+    loadSVG("./Svgs/letters/字母I.svg")
+    loadSVG("./Svgs/letters/字母L.svg")
+    loadSVG("./Svgs/letters/字母R.svg")
+    loadSVG("./Svgs/letters/字母T.svg")
+  ]
+}
+
 function setup() {
   createCanvas(400, 400, SVG);
   addDownloadButton();
+
+
   noLoop(); // Opzionale
 }
 
