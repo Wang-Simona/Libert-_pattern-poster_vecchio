bddcddae4155d8c073fdb7dfe2a90dc3d2e5a246
Wang-Simona <wyingxuan9@gmail.com>
2025-05-27 11:17:15 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,35 +5,34 @@ let lettere_svg = [];
 let forme_svg = [];
 
 function preload() {
-  lettere_svg = [
-    loadSVG("./Svgs/shapes/1.svg")
-    loadSVG("./Svgs/shapes/6.svg")
-    loadSVG("./Svgs/shapes/11.svg")
-    loadSVG("./Svgs/shapes/16.svg")
-    loadSVG("./Svgs/shapes/21.svg")
-    loadSVG("./Svgs/shapes/26.svg")
-    loadSVG("./Svgs/shapes/31.svg")
-    loadSVG("./Svgs/shapes/36.svg")
-    loadSVG("./Svgs/shapes/41.svg")
-    loadSVG("./Svgs/shapes/46.svg")
-    loadSVG("./Svgs/shapes/51.svg")
-    loadSVG("./Svgs/shapes/56.svg")
-    loadSVG("./Svgs/shapes/61.svg")
-    loadSVG("./Svgs/shapes/66.svg")
-    loadSVG("./Svgs/shapes/71.svg")
-    
+  forme_svg = [
+    loadSVG("./Svgs/shapes/1.svg"),
+    loadSVG("./Svgs/shapes/6.svg"),
+    loadSVG("./Svgs/shapes/11.svg"),
+    loadSVG("./Svgs/shapes/16.svg"),
+    loadSVG("./Svgs/shapes/21.svg"),
+    loadSVG("./Svgs/shapes/26.svg"),
+    loadSVG("./Svgs/shapes/31.svg"),
+    loadSVG("./Svgs/shapes/36.svg"),
+    loadSVG("./Svgs/shapes/41.svg"),
+    loadSVG("./Svgs/shapes/46.svg"),
+    loadSVG("./Svgs/shapes/51.svg"),
+    loadSVG("./Svgs/shapes/56.svg"),
+    loadSVG("./Svgs/shapes/61.svg"),
+    loadSVG("./Svgs/shapes/66.svg"),
+    loadSVG("./Svgs/shapes/71.svg"),
   ]
 }
 
 function preload() {
-  forme_svg = [
-    loadSVG("./Svgs/letters/字母à.svg")
-    loadSVG("./Svgs/letters/字母B.svg")
-    loadSVG("./Svgs/letters/字母E.svg")
-    loadSVG("./Svgs/letters/字母I.svg")
-    loadSVG("./Svgs/letters/字母L.svg")
-    loadSVG("./Svgs/letters/字母R.svg")
-    loadSVG("./Svgs/letters/字母T.svg")
+  lettere_svg = [
+    loadSVG("./Svgs/letters/字母à.svg"),
+    loadSVG("./Svgs/letters/字母B.svg"),
+    loadSVG("./Svgs/letters/字母E.svg"),
+    loadSVG("./Svgs/letters/字母I.svg"),
+    loadSVG("./Svgs/letters/字母L.svg"),
+    loadSVG("./Svgs/letters/字母R.svg"),
+    loadSVG("./Svgs/letters/字母T.svg"),
   ]
 }
 
@@ -47,4 +46,6 @@ function setup() {
 
 function draw() {
   clear(); // Non cancellare!
+  console.log(svg)
+  image(lettere_svg,0,0)
 }
