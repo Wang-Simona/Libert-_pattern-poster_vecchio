bf8dae91dfe1283dd5e6a1d1f78dcca0486fb982
Wang-Simona <wyingxuan9@gmail.com>
2025-05-27 13:03:58 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -53,11 +53,11 @@ function draw() {
 
   //let randomsopra = lettere_A,lettere_B,lettere_E,lettere_I,lettere_L,lettere_R,lettere_T;
   //fillSVG(randomsopra,"green");
-  image(lettere_L, width/4-50,height/3-50, 100, 100);
-  image(lettere_I, width/4+50,height/3-50, 100, 100);
-  image(lettere_B, width/4+50,height/3+50, 100, 100);
-  image(lettere_E, width/4+150,height/3+50, 100, 100);
-  image(lettere_R, width/4+250,height/3+50, 100, 100);
-  image(lettere_T, width/4+150,height/3+150, 100, 100);
-  image(lettere_A, width/4+250,height/3+150, 100, 100);
+  image(lettere_L, width/4-50,height/3-60, 100, 100);
+  image(lettere_I, width/4+50,height/3-60, 100, 100);
+  image(lettere_B, width/4+50,height/2, 100, 100);
+  image(lettere_E, width/4+150,height/2, 100, 100);
+  image(lettere_R, width/4+250,height/2, 100, 100);
+  image(lettere_T, width/4+150,height-60, 100, 100);
+  image(lettere_A, width/4+250,height-60, 100, 100);
 }
